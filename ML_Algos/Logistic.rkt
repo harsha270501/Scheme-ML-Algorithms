da0a0b55e1e5c8cddb22f5047d128da6b92710ff
harsha270501 <harshavardhinibalakrishnan@gmail.com>
2021-11-16 10:44:46 +0530

    Logistic - completed

--- ML_Algos/Logistic.rkt
+++ ML_Algos/Logistic.rkt
@@ -8,33 +8,33 @@
         (+ (* (car row) (car coeff)) (predict-helper (cdr row) (cdr coeff)))))
   (define (predict-main)
     (+ (car coeff) (predict-helper (cdr row) (cdr coeff))))
-  (if (> (predict-main) 0) 1
-      0))
+  (/ 1 (+ 1 (exp (- (predict-main)))))
+  )
 
 
 
-(define (train-weights train lrate nepochs)
+(define (train-coef train lrate nepochs)
 
-  (define (update-weights wt row err)
+  (define (update-coef wt yhat row err)
     (if (or (null? row) (null? wt))
         '()
-        (cons (+ (car wt) (* err lrate (car row))) (update-weights (cdr wt) (cdr row) err))))
+        (cons (+ (car wt) (* err lrate yhat (- 1 yhat) (car row))) (update-coef (cdr wt) yhat (cdr row) err))))
   
-  (define (init-weights l)
+  (define (init-coef l)
     (if (= l 0) '()
-        (cons 0 (init-weights (- l 1)))))
+        (cons 0 (init-coef (- l 1)))))
   
-  (define weights (init-weights (length (car train))))
+  (define coef (init-coef (length (car train))))
 
   (define (train-epoch wt np)
     (if (= np 0) wt
         (train-epoch (train-epoch-all-rows train wt) (- np 1))))
   
   (define (train-epoch-row row wt)
-    (let ((pred (predict row wt))
+    (let ((yhat (predict row wt))
           (class (get-class row)))
-      (let ((err (- class pred)))
-        (cons (* (car wt) lrate err) (update-weights (cdr wt) row err))
+      (let ((err (- class yhat)))
+        (cons (* (car wt) lrate err yhat (- 1 yhat)) (update-coef (cdr wt) yhat row err))
         )))
 
   
@@ -42,13 +42,13 @@
     (if (null? df) wt
         (train-epoch-all-rows (cdr df) (train-epoch-row (car df) wt))))
 
-  (train-epoch weights nepochs)
+  (train-epoch coef nepochs)
 )
 
-(trace-define (perceptron train x_test args)
+(trace-define (logistic train x_test args)
   (trace-define (predict-test wts xtest)
     (if (null? xtest) '()
         (cons (predict (car xtest) wts) (predict-test wts (cdr xtest)))))
   
-  (let ((trained_wts (train-weights train (car args) (cdr args))))
-    (predict-test trained_wts x_test)))
+  (let ((trained_coef (train-coef train (car args) (cdr args))))
+    (predict-test trained_coef x_test)))
